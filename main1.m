e47c7539844f150c3c3e4a47af34fcb92ea7a180
seibolda <ga34daq@mytum.de>
2017-01-25 11:39:20 +0100

    vectorized computation; no inner loops any more; set figures to specific position;

--- main1.m
+++ main1.m
@@ -1,65 +1,117 @@
-silhouette = double(rgb2gray(imread('data/silhouettes/wateringcan_sil_med.png'))) > 0;
+close all;
+clear;
+
+silhouette = double(rgb2gray(imread('data/silhouettes/wateringcan_sil_med.png'))) > 1;
 img = double(rgb2gray(imread('data/images/wateringcan_med.png')))/255;
 
+addpath(genpath('minFunc_2012'));
+addpath(genpath('Pottslab0.42'));
+%setPLJavaPath(true);
+
+%figures
+fig1 = figure(1);
+set(fig1, 'Name', 'u_k_plus_1 initial', 'OuterPosition', [0, 500, 550, 500]);
+fig2 = figure(2);
+set(fig2, 'Name', 'c_k_plus_1 initial', 'OuterPosition', [800, 500, 100, 100]);
+fig3 = figure(3);
+set(fig3, 'Name', 'u_k_plus_1_shading optimized', 'OuterPosition', [0, 0, 550, 500]);
+fig4 = figure(4);
+set(fig4, 'Name', 'c_k_plus_1_shading optimized', 'OuterPosition', [800, 0, 100, 100]);
 
 
 % surface
 [m, n] = size(silhouette);
 lambda = 0.01;
-Vol = 1000;
+Vol = 100000;
 grad = build_grad(m, n);
 grad([silhouette(:)==0;silhouette(:)==0],:) = 0;
 u_tilde_k_plus_1 = zeros(m*n,1); % set constant for now
-tau_u = 10;
+tau_u = 1000;
+tau_u2 = 0.1;
 
 u_k_plus_1 = solve_min_surface(grad, silhouette(:), lambda, Vol, u_tilde_k_plus_1, tau_u);
 
-figure;
-surf(reshape(u_k_plus_1,m,n));
 
+
+figure(1);
+surfl(reshape(u_k_plus_1,m,n));
+shading flat;
+axis equal
+colormap gray;
+view(145,30);
+camlight(0,-90)
+camlight headlight
 
 
 % PottsLab
-setPLJavaPath(true);
 tau_c = 0.01;
-gamma = 0.1;
-c_tilde_k_plus_1 = img .* silhouette; % initialize with image
+tau_c2 = 100;
+gamma = 10;
 
-c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
+c_tilde_k_plus_1 = silhouette;
+%~ c_tilde_k_plus_1 = silhouette.*img; % initialize with image
+
+c_k_plus_1 = c_tilde_k_plus_1;
+%~ c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, 1e-9*tau_c, gamma);
+
+
 figure(2);
 imshow(c_k_plus_1);
 
 
 
-% do step
-l = [1,0,-1];
-img_vec = img(:);
 
-for i = 0:30
+% lighting vector
+l = [1,0,1];
+
+% Divergence
+div_x = -transpose(grad(1:m*n,:));
+div_y = -transpose(grad(m*n+1:end,:));
+
+for i = 0:100
+	% Shading
     grad_sil = grad * u_k_plus_1(:);
 
-    for x = 1:m
-        for y = 0:(n-1)
-            idx = x + y*m;
-            grad_norm = sqrt(grad_sil(idx)^2 + grad_sil(idx + m*n)^2 + 1);
-            k = c_k_plus_1(idx) / grad_norm;
-            p = [grad_sil(idx), grad_sil(idx + m*n), -1];
-            brackets = (img_vec(idx) - (dot(p, l) / grad_norm) * c_k_plus_1(idx));
-
-            u_tilde_k_plus_1(idx) = u_k_plus_1(idx) + ([grad(idx); grad(idx + m*n)]' * [brackets * (l(1) * k); brackets * (l(2) * k)]);
-            c_tilde_k_plus_1(idx) = c_k_plus_1(idx) + brackets * ((dot(p,l) / grad_norm));
-        end
-    end
-
-    u_k_plus_1 = solve_min_surface(grad, silhouette(:), lambda, Vol, u_tilde_k_plus_1, tau_u);
+	p1 = reshape(grad_sil(1:m*n),m,n);
+	p2 = reshape(grad_sil(m*n+1:end),m,n);
+	grad_norm = p1.^2+p2.^2+1;
+	k = c_k_plus_1 ./ grad_norm;
+	
+	l_dot_p = (-l(1)*p1-l(2)*p2+l(3));
+	
+	brackets = (img-l_dot_p./grad_norm.*c_k_plus_1);
+	brackets = brackets.*silhouette;
+	
+	c_tilde_k_plus_1 = c_k_plus_1  + (1./tau_c2)*brackets.*l_dot_p./ grad_norm;
+	
+	u_k_plus_1_shading = u_k_plus_1 + (div_x*(brackets(:) .* (l(1) .* k(:))) + div_y*(brackets(:) .* (l(2) .* k(:))))./tau_u2; 
+	
+    
+    
+    
+	figure(3)
+    surfl(reshape(u_k_plus_1_shading,m,n));
+    shading flat;
+    colormap gray;
+    axis equal
+    view(145,30);
+	camlight(0,-90)
+	camlight headlight
+		
+	% Min Surface
+    u_k_plus_1 = solve_min_surface(grad, silhouette(:), lambda, Vol, u_k_plus_1_shading, tau_u);
+    
+    % Potts
     c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
     
-    figure(3);
-    surf(reshape(u_tilde_k_plus_1,m,n));
-    shading flat;
+    
+    
+    
     figure(4);
     imshow(reshape(c_tilde_k_plus_1,m,n));
     drawnow;
+    
+    
 end;
 
 
