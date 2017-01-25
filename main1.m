cfc3d56877c053ea3739e9b62d6dc2d91539e25c
seibolda <ga34daq@mytum.de>
2017-01-25 10:10:27 +0100

    corrected some errors in loop computation

--- main1.m
+++ main1.m
@@ -26,38 +26,40 @@ gamma = 0.1;
 c_tilde_k_plus_1 = img .* silhouette; % initialize with image
 
 c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
-figure;
+figure(2);
 imshow(c_k_plus_1);
 
 
 
 % do step
-grad_sil = grad * u_k_plus_1(:);
-l = [1,0,1];
-c = img(:);
+l = [1,0,-1];
 img_vec = img(:);
 
-for i = 0:100
+for i = 0:30
+    grad_sil = grad * u_k_plus_1(:);
 
     for x = 1:m
         for y = 0:(n-1)
             idx = x + y*m;
             grad_norm = sqrt(grad_sil(idx)^2 + grad_sil(idx + m*n)^2 + 1);
-            k = c(idx) / grad_norm;
+            k = c_k_plus_1(idx) / grad_norm;
             p = [grad_sil(idx), grad_sil(idx + m*n), -1];
-            brackets = (img_vec(idx) - (dot(p, l) / grad_norm) * c(idx));
+            brackets = (img_vec(idx) - (dot(p, l) / grad_norm) * c_k_plus_1(idx));
 
-            u_tilde_k_plus_1(idx) = u_k_plus_1(idx) - ([grad(idx); grad(idx + m*n)]' * [brackets * (l(1) * k); brackets * (l(2) * k)]);
-            c_tilde_k_plus_1(idx) = c_k_plus_1(idx) + brackets * ((dot(p,l) / grad_norm) * c_k_plus_1(idx));
+            u_tilde_k_plus_1(idx) = u_k_plus_1(idx) + ([grad(idx); grad(idx + m*n)]' * [brackets * (l(1) * k); brackets * (l(2) * k)]);
+            c_tilde_k_plus_1(idx) = c_k_plus_1(idx) + brackets * ((dot(p,l) / grad_norm));
         end
     end
 
     u_k_plus_1 = solve_min_surface(grad, silhouette(:), lambda, Vol, u_tilde_k_plus_1, tau_u);
     c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
     
+    figure(3);
+    surf(reshape(u_tilde_k_plus_1,m,n));
+    shading flat;
+    figure(4);
+    imshow(reshape(c_tilde_k_plus_1,m,n));
+    drawnow;
 end;
 
-figure;
-surf(reshape(u_tilde_k_plus_1,m,n));
-figure;
-imshow(reshape(c_tilde_k_plus_1,m,n));
+
