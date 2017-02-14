78a71e7fb10f8d7c62989b569b08f104ad38b912
seibolda <ga34daq@mytum.de>
2017-02-14 11:20:14 +0100

    weight for minimal surface smoothing term; line search working now

--- compute_shading.m
+++ compute_shading.m
@@ -1,4 +1,4 @@
-function [brackets, shading_grad_u, shading_grad_c] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l)
+function [brackets, shading_grad_u, shading_grad_c] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l, alpha)
 
 [m,n] = size(silhouette);
 
@@ -9,12 +9,12 @@ p2 = reshape(grad_sil(m*n+1:end), m, n);
 grad_norm = sqrt(p1.^2 + p2.^2 + 1);
 k = c_k_plus_1 ./ grad_norm;
 
-l_dot_p = (l(1)*p1 + l(2)*p2 - l(3));
+l_dot_p = (l(1)*p1 + l(2)*p2 + l(3));
 
 brackets = (img - (l_dot_p./grad_norm) .* c_k_plus_1);
 brackets = brackets.*silhouette;
 
-shading_grad_c = brackets.*(l_dot_p./ grad_norm);
-shading_grad_u = (div_x*(brackets(:) .* (l(1) .* k(:))) + div_y*(brackets(:) .* (l(2) .* k(:))));
+shading_grad_c = alpha * (brackets.*(l_dot_p./ grad_norm));
+shading_grad_u = alpha * (div_x*(brackets(:) .* (l(1) .* k(:))) + div_y*(brackets(:) .* (l(2) .* k(:))));
 
 end
--- main1.m
+++ main1.m
@@ -2,8 +2,8 @@ close all;
 clear;
 
 % load data
-silhouette = double(rgb2gray(imread('data/silhouettes/wateringcan_sil_med.png'))) > 1;
-img = double(rgb2gray(imread('data/images/wateringcan_med.png')))/255;
+silhouette = double(rgb2gray(imread('data/silhouettes/archutah_sil_med.png'))) > 1;
+img = double(rgb2gray(imread('data/images/archutah_med.png')))/255;
 
 addpath(genpath('minFunc_2012'));
 addpath(genpath('Pottslab0.42'));
@@ -24,7 +24,8 @@ set(fig4, 'Name', 'c_k_plus_1_shading optimized', 'OuterPosition', [800, 0, 100,
 
 % for surface
 [m, n] = size(silhouette);
-lambda = 0.01;
+minimal_surface_weight = 0.5;
+lambda = 0.01/minimal_surface_weight;
 Vol = 100000;
 tau_u = 10;
 grad = build_grad(m, n);
@@ -33,9 +34,9 @@ div_x = -transpose(grad(1:m*n,:)); % Divergence x
 div_y = -transpose(grad(m*n+1:end,:)); % Divergence y
 % for pottslab
 tau_c = 1;
-gamma = 25;
+gamma = 0.1/minimal_surface_weight;
 % for rest
-alpha = 1;
+alpha = 1/minimal_surface_weight;
 l = [1,0,1]; % lighting vector
 eta = 0.5;
 
@@ -69,15 +70,15 @@ c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
 
 
 figure(2);
-imshow(c_k_plus_1);
+imshow(c_k_plus_1,[]);
 
 
 
 
 
-for iteration = 0:100
+for iteration_k = 0:100
     
-    [computed_shading_k, shading_grad_u, shading_grad_c] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l);
+    [shading_energy_k, shading_grad_u_k, shading_grad_c_k] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l, alpha);
     u_k = u_k_plus_1;
     c_k = c_k_plus_1;
     
@@ -85,8 +86,8 @@ for iteration = 0:100
     while true
         
         % Shading
-        c_tilde_k_plus_1 = c_k + alpha*(shading_grad_c*tau_c);
-        u_tilde_k_plus_1 = u_k + alpha*(shading_grad_u.*tau_u); 
+        c_tilde_k_plus_1 = c_k + shading_grad_c_k*tau_c;
+        u_tilde_k_plus_1 = u_k - shading_grad_u_k.*tau_u; 
 
         
         % Min Surface
@@ -96,11 +97,11 @@ for iteration = 0:100
         c_k_plus_1 = solve_potts_model(c_tilde_k_plus_1, tau_c, gamma);
         
         % shading
-        [computed_shading, shading_grad_u, shading_grad_c] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l);
+        [computed_shading, ~, ~] = compute_shading(u_k_plus_1, c_k_plus_1, grad, div_x, div_y, silhouette, img, l, alpha);
     
         % g_x = sum(sqrt(1 + grad_u(1:m*n).^2 + grad_u(m*n+1:end).^2)) + lambda * (sum(silhouette(:).*u_k_plus_1) - Vol)^2 + gamma * sum(abs(grad_c) > 1e-6);
         % check if step size tau_x is good
-        [Q_L, p_L] = compute_correct_step_size(computed_shading, computed_shading_k, shading_grad_u, shading_grad_c, ...
+        [Q_L, p_L] = compute_correct_step_size(computed_shading, shading_energy_k, shading_grad_u_k, shading_grad_c_k, ...
             u_k_plus_1, u_k, c_k_plus_1, c_k, tau_u, tau_c, alpha);
         
         fprintf('p_L: %d Q_L: %d tau_u: %d tau_c: %d\n', p_L, Q_L, tau_u, tau_c);
@@ -127,7 +128,7 @@ for iteration = 0:100
 	camlight headlight
     
     figure(4);
-    imshow(reshape(c_tilde_k_plus_1,m,n));
+    imshow(reshape(c_k_plus_1,m,n),[]);
     drawnow;
     
     
