c460470c325190a9aa2efbb676419be58dbef723
seibolda <ga34daq@mytum.de>
2017-07-27 21:49:56 +0200

    updated cuda_potts_solver renamed cuda_pottslab renamed pottslabTest

--- cuda_potts_solver.m
+++ cuda_potts_solver.m
@@ -6,17 +6,18 @@ function result = cuda_pottslab(input, gamma, varargin)
 
 % parse options
 ip = inputParser;
-addParamValue(ip, 'muStep', 2);
-addParamValue(ip, 'stopTol', 1e-10);
-addParamValue(ip, 'isotropic', true);
-addParamValue(ip, 'verbose', false);
-addParamValue(ip, 'isGPU', true);
-addParamValue(ip, 'chunkSize', 0);
-addParamValue(ip, 'chunkOffsetChangeType', 0);
-addParamValue(ip, 'quadraticError', true);
-addParamValue(ip, 'maxIterations', 50);
-addParamValue(ip, 'xblocksize', 256);
-addParamValue(ip, 'yblocksize', 4);
+addParameter(ip, 'muStep', 2);
+addParameter(ip, 'stopTol', 1e-10);
+addParameter(ip, 'isotropic', true);
+addParameter(ip, 'verbose', false);
+addParameter(ip, 'isGPU', true);
+addParameter(ip, 'chunkSize', 0);
+addParameter(ip, 'chunkOffsetChangeType', 0);
+addParameter(ip, 'quadraticError', true);
+addParameter(ip, 'maxIterations', 50);
+addParameter(ip, 'xblocksize', 256);
+addParameter(ip, 'yblocksize', 4);
+addParameter(ip, 'deviceNumber', 0);
 
 parse(ip, varargin{:});
 par = ip.Results;
@@ -25,4 +26,4 @@ par = ip.Results;
 assert(par.muStep > 1, 'Variable muStep must be > 1.');
 assert(par.stopTol > 0, 'Stopping tolerance must be > 0.');
 
-result = main_mex(input, gamma, par.muStep, par.chunkSize, par.stopTol, par.chunkOffsetChangeType, par.maxIterations, par.verbose, par.quadraticError, par.isotropic, par.isGPU, par.xblocksize, par.yblocksize);
+result = main_mex(input, gamma, par.muStep, par.chunkSize, par.stopTol, par.chunkOffsetChangeType, par.maxIterations, par.verbose, par.quadraticError, par.isotropic, par.isGPU, par.xblocksize, par.yblocksize, par.deviceNumber);
--- pottsSolverTest.m
+++ pottsSolverTest.m
@@ -5,7 +5,7 @@ addpath(genpath('Pottslab0.42'));
 %fig1 = figure(1);
 %set(fig1, 'Name', 'result', 'OuterPosition', [0, 600, 550, 500]);
 
-imgOrg = single(double(imread('data/images/archutah_med.png'))) / 255;
+imgOrg = single(double(imread('data/images/coffee2_sm.png'))) / 255;
 gamma = 0.1;
 
 %nBins = 255;
@@ -19,7 +19,7 @@ gamma = 0.1;
 %
 
 %f = @() minL2Potts2DADMM(imgOrg, gamma, 'verbose', false, 'isotropic', true, 'multiThreading', true);
-f = @() cuda_pottslab(imgOrg, gamma, 'verbose', false, 'isGPU', true);
+f = @() cuda_potts_solver(imgOrg, gamma, 'verbose', false, 'isGPU', true);
 
 %result = minL2Potts2DADMM(imgOrg, gamma, 'verbose', false, 'isotropic', true, 'multiThreading', false);
 %result = cuda_pottslab(imgOrg, gamma, 'verbose', false, 'isGPU', false);
